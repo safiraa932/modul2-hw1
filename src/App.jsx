111dc8d6cb20c2aca5f8c1c7f0660496b9212ba2
safiraa932 <merlansafira3@gmail.com>
2022-03-23 00:18:45 +0700

    first commit react

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+import data from './data';
+
+class Album extends Component {
+  componentDidMount() {
+    try {
+      console.log(data);
+  
+      const imgAlbum = document.getElementById("img-album");
+      const albumTitle = document.getElementById("album-title");
+      const artistName = document.getElementById("artist-name");
+      const releaseDate = document.getElementById("release-date");
+      const totalTracks = document.getElementById("total-tracks");
+  
+      imgAlbum.src = data.album.images[1].url;
+      albumTitle.innerText = data.album.name;
+      artistName.innerText = data.artists[0].name;
+      releaseDate.innerText = data.album.release_date;
+      totalTracks.innerText = data.album.total_tracks;
+      
+    } catch(err) {
+      alert(err.message);
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <section className="suggested-section">
+          <div className="layout">
+            <div className="suggested-list">
+              <div className="card-suggested">
+                <div id="img-album">
+                  <img src="https://i.scdn.co/image/ab67616d0000b273e8b066f70c206551210d902b" />
+                </div>
+                <div id="album-detail card-content">
+                  <h3>Album</h3>
+                  <h1 id="album-title"></h1>
+                  <div id="artist-info">
+                    <p>
+                      Artist : <span id="artist-name"></span>
+                    </p>
+                    <p>
+                      Release Date : <span id="release-date"></span>
+                    </p>
+                    <p>
+                      Total Tracks : <span id="total-tracks"></span>
+                    </p>
+                    <button className="btn bg-secondary">Select</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+function App() {
+  return (
+    <div className="App">
+      <Album />
+    </div>
+  );
+}
+
+export default App;
