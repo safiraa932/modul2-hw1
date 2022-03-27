b4fe0c17d2c7a08899862c117b9ccd5ac4701629
safiraa932 <merlansafira3@gmail.com>
2022-03-28 00:41:45 +0700

    loop data

--- src/components/Album/index.js
+++ src/components/Album/index.js
@@ -1,43 +1,43 @@
 import React, { useEffect } from "react";
 
 const Album = ({ data }) => {
-  useEffect(() => {
-    try {
-      console.log(data);
+  // useEffect(() => {
+  //   try {
+  //     console.log(data);
 
-      const imgAlbum = document.getElementById("img-album");
-      const albumTitle = document.getElementById("album-title");
-      const artistName = document.getElementById("artist-name");
-      const releaseDate = document.getElementById("release-date");
-      const totalTracks = document.getElementById("total-tracks");
+  //     const imgAlbum = document.getElementById("img-album");
+  //     const albumTitle = document.getElementById("album-title");
+  //     const artistName = document.getElementById("artist-name");
+  //     const releaseDate = document.getElementById("release-date");
+  //     const totalTracks = document.getElementById("total-tracks");
 
-      imgAlbum.src = data.album.images[1].url;
-      albumTitle.innerText = data.album.name;
-      artistName.innerText = data.artists[0].name;
-      releaseDate.innerText = data.album.release_date;
-      totalTracks.innerText = data.album.total_tracks;
-    } catch (err) {
-      alert(err.message);
-    }
-  }, []);
-
+  //     imgAlbum.src = data.album.images[1].url;
+  //     albumTitle.innerText = data.album.name;
+  //     artistName.innerText = data.artists[0].name;
+  //     releaseDate.innerText = data.album.release_date;
+  //     totalTracks.innerText = data.album.total_tracks;
+  //   } catch (err) {
+  //     alert(err.message);
+  //   }
+  // }, []);
+console.log(data.album);
   return (
     <div className="card-suggested">
       <div id="img-album">
-        <img src="https://i.scdn.co/image/ab67616d0000b273e8b066f70c206551210d902b" />
+        {/* <img src="https://i.scdn.co/image/ab67616d0000b273e8b066f70c206551210d902b" /> */}
+        <img src={data.album.images[0].url} alt={data.title}/>
       </div>
       <div id="album-detail card-content">
-        <h3>Album</h3>
-        <h1 id="album-title"></h1>
+        <h1 id="album-title">{data.album.name}</h1>
         <div id="artist-info">
           <p>
-            Artist : <span id="artist-name"></span>
+            Artist : <span id="artist-name">{data.artists[0].name}</span>
           </p>
           <p>
-            Release Date : <span id="release-date"></span>
+            Release Date : <span id="release-date">{data.album.release_date}</span>
           </p>
           <p>
-            Total Tracks : <span id="total-tracks"></span>
+            Total Tracks : <span id="total-tracks">{data.album.total_tracks}</span>
           </p>
           <button className="btn bg-secondary">Select</button>
         </div>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,17 +3,19 @@ import Album from "../../components/Album/index";
 import data from "../../data";
 
 const Home = () => {
-    return (
-        <>
-          <section className="suggested-section">
-            <div className="layout">
-                <div className="suggested-list">
-                    <Album data = { data }/>
-                </div>
-            </div>
-          </section>
-        </>
-    );
+  return (
+    <>
+      <section className="suggested-section">
+        <div className="layout">
+          <div className="suggested-list">
+            {/* <Album data = { data }/> */}
+            {data.map((item) => (
+              <Album key={item.id} data={item} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
-
 export default Home;
